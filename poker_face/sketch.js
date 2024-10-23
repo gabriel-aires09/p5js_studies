b9cb2e97da036da5e506784a85e0f74e5de67fdf
gabriel-aires09 <aires514@gmail.com>
2024-10-23 11:46:15 -0300

    initial commit

---
+++ poker_face/sketch.js
@@ -0,0 +1,24 @@
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(135, 206, 220);
+  
+  fill("rgb(227,227,61)");
+  stroke("black");
+  strokeWeight(5);
+  circle(320, 200, 100);
+  
+  fill("black");
+  circle(300,189, 10);
+  
+  fill("black");
+  circle(350, 189, 10);
+  
+  fill("black");
+  line(310, 220, 340, 215);
+  stroke("black");
+  strokeWeight(5);
+  
+}
